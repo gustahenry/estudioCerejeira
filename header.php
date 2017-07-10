800cb82ac2ca264cdb85aa2e84b366e6f1e7eeab
Gustavo <gustavah_and1@hotmail.com>
2017-07-10 19:41:02 -0300

    construcao da home page

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="css/bootstrap.min.css?version=4" rel="stylesheet">
+    <link rel="stylesheet" href="css/custom.css?version=1">
+  </head>
+  <body>
+    
+    
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php  require_once('header.php') ?>
+<div class="container">
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#"><span class="brand1">ESTUDIO</span><span class="brand2">CEREJEIRA</span></a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">SOBRE</a></li>
+	        <li><a href="#">PORTFÓLIO</a></li>
+	        <li><a id="contato" href="#">CONTATO</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
+	<div class="jumbotron text-center">
+	  <h1>Hello, world!</h1>
+	  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+	  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+	</div>
+</div>
+
+
+<?php require_once('footer.php') ?>
