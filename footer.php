c7b6f69d09dd85b64cf7a49bcf7bdd2116b5136c
Gustavo <gustavah_and1@hotmail.com>
2017-08-24 00:06:52 -0300

    atualizacao footer

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<div class="footers"></div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/custon.js"></script>
 <script src="js/bootstrap.min.js"></script>
