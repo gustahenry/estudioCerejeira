13280b107e1cbfe9cc208430681ece3f84c4665e
gustavoDesen <pcasemirog@gmail.com>
2017-09-05 19:07:32 -0300

    Update contato.php
    Atualização do email

--- contato.php
+++ contato.php
@@ -69,7 +69,7 @@
 			<div class="informacoes">
 				<span class="brand1">FALE COM A GENTE</span><br />
 				+55 11 95461-7360<br />
-				contato@estudiocerejeira.com.br
+				@estudiocerejeira.com.br
 			</div>
 			
 		</div>
