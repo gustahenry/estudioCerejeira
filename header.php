6322294f719546322ee6f1879a01f872048cfebd
gustavoDesen <pcasemirog@gmail.com>
2017-09-05 19:41:07 -0300

    Update header.php
    inclusao da imagem para compartilhamento

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta property="og:title" content="Estudio Cerejeira" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://estudiocerejeira.com.br/" />
+    <meta property="og:image" content="http://estudiocerejeira.com.br/img/logo-site.png" />
     <title>ESTUDIO CEREJEIRA</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="css/bootstrap.min.css?version=7" rel="stylesheet">
