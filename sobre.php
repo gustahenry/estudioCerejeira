650d6e7f6b29a52c5905cfb89edc596789e9acf3
gustavo.henrique <pcasemirog@gmail.com>
2018-02-15 15:48:56 -0200

    inclusao do svg e alteracao no site todo

--- sobre.php
+++ sobre.php
@@ -60,9 +60,50 @@
 			<p>Formada em Comunicação Social - Jornalismo pela Universidade Anhembi Morumbi, Raquel resolveu unir todos os aprendizados dos mais de 10 anos de profissão na área de reportagem e produção para televisão, conteúdo para sites e revistas, comunicação interna, assessoria de imprensa e marketing - com passagem em grandes empresas como Record TV, Vevo, Daslu, Editora Símbolo e Infracommerce - para criar sua própria produtora de vídeos.</p>
 
 			<div class="midia2">
-				<a href="https://www.facebook.com/estudiocerejeira" target="_blank" ><img class="midia-groups2" src="img/facebook.png" alt=""></a>
-					<a href="https://www.instagram.com/estudiocerejeira" target="_blank"><img class="midia-groups2" src="img/instagram.png" alt=""></a>
-					<a href=""><img class="midia-groups2" src="img/vimeo.png" alt=""></a>
+				<a href="https://www.facebook.com/estudiocerejeira" target="_blank" >
+						<svg width="38px" height="38px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 20.3 20.3" style="enable-background:new 0 0 20.3 20.3;" xml:space="preserve">
+						<style type="text/css">
+							.st0{fill:#C64167;}
+							.st1{fill:#FFFFFF;}
+						</style>
+						<path class="st0" d="M20.3,10.2c0,5.6-4.6,10.2-10.2,10.2C4.6,20.3,0,15.8,0,10.2C0,4.6,4.6,0,10.2,0C15.8,0,20.3,4.6,20.3,10.2z"/>
+						<path class="st1" d="M11,15.9v-5.2h1.7l0.3-2h-2V7.3c0-0.6,0.2-1,1-1h1.1V4.6c-0.2,0-0.8-0.1-1.6-0.1C10,4.5,9,5.4,9,7.1v1.5H7.2v2
+							H9v5.2H11z"/>
+						</svg>
+					</a>
+					<a href="https://www.instagram.com/estudiocerejeira" target="_blank">
+						<svg width="38px" height="38px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+							 viewBox="0 0 20.3 20.3" style="enable-background:new 0 0 20.3 20.3;" xml:space="preserve">
+						<style type="text/css">
+							.st0{fill:#C64167;}
+							.st1{fill:#FFFFFF;}
+						</style>
+						<path class="st0" d="M20.3,10.2c0,5.6-4.6,10.2-10.2,10.2C4.6,20.3,0,15.8,0,10.2C0,4.6,4.6,0,10.2,0C15.8,0,20.3,4.6,20.3,10.2z"/>
+						<g>
+							<path class="st1" d="M12.5,4.5H7.9c-1.8,0-3.3,1.5-3.3,3.3v4.9c0,1.8,1.5,3.3,3.3,3.3h4.6c1.8,0,3.3-1.5,3.3-3.3V7.7
+								C15.7,5.9,14.2,4.5,12.5,4.5z M14.9,12.6c0,1.3-1.1,2.4-2.4,2.4H7.9c-1.3,0-2.4-1.1-2.4-2.4V7.7c0-1.3,1.1-2.4,2.4-2.4h4.6
+								c1.3,0,2.4,1.1,2.4,2.4V12.6z"/>
+							<path class="st1" d="M10.2,7.1C10.2,7.1,10.1,7.1,10.2,7.1C9.3,7.1,8.6,7.4,8,8c-0.6,0.6-0.9,1.3-0.9,2.1c0,1.7,1.4,3,3,3
+								c0.8,0,1.6-0.3,2.1-0.9c0.6-0.6,0.9-1.3,0.9-2.1c0-0.8-0.2-1.6-0.7-2.1C11.9,7.5,11.1,7.2,10.2,7.1z M10.2,12.2c-1.1,0-2-0.9-2-2
+								c0-1.1,0.9-2,1.9-2c0,0,0,0,0.1,0c1.3,0,2,0.8,2,2C12.1,11.3,11.3,12.2,10.2,12.2z"/>
+							<path class="st1" d="M13.3,6.3c-0.4,0-0.7,0.3-0.7,0.7c0,0.4,0.3,0.7,0.7,0.7C13.7,7.7,14,7.4,14,7C14,6.7,13.7,6.3,13.3,6.3z"/>
+						</g>
+						</svg>
+					</a>
+					<a href="">
+						<svg width="38px" height="38px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+							 viewBox="0 0 20.3 20.3" style="enable-background:new 0 0 20.3 20.3;" xml:space="preserve">
+						<style type="text/css">
+							.st0{fill:#C64167;}
+							.st1{fill-rule:evenodd;clip-rule:evenodd;fill:#FFFFFF;}
+						</style>
+						<path class="st0" d="M20.3,10.2c0,5.6-4.6,10.2-10.2,10.2C4.6,20.3,0,15.8,0,10.2C0,4.6,4.6,0,10.2,0C15.8,0,20.3,4.6,20.3,10.2z"/>
+						<path class="st1" d="M16.2,6.9c0.1-0.5,0.1-1-0.2-1.4c-0.4-0.6-1.4-0.6-2-0.5c-0.5,0.1-2.3,0.9-2.9,2.7c1.1-0.1,1.6,0.1,1.5,1.2
+							c0,0.5-0.3,1-0.6,1.5c-0.3,0.6-0.9,1.8-1.7,0.9c-0.7-0.8-0.7-2.2-0.8-3.2C9.4,7.7,9.3,7,9.2,6.5C9,6,8.7,5.4,8.2,5.3
+							c-0.5-0.1-1,0.1-1.4,0.3C5.8,6.2,5,7.1,4.1,7.8v0.1C4.3,8,4.3,8.3,4.6,8.4c0.6,0.1,1.2-0.6,1.7,0.1c0.3,0.4,0.3,0.9,0.5,1.4
+							c0.2,0.6,0.4,1.3,0.6,2c0.3,1.2,0.7,3,1.7,3.5c0.5,0.2,1.3-0.1,1.7-0.3c1.1-0.7,2-1.6,2.7-2.6C15.1,10.2,16.1,7.6,16.2,6.9z"/>
+						</svg>
+					</a>
 				</div>
 		</div>
 		<div class="col-sm-1"></div>
